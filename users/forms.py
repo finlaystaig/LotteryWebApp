2ef8f71123365ecde07ce34fbfa5bb95372499e2
finlay <F.Staig1@newcastle.ac.uk>
2021-10-20 17:05:43 +0100

    added some input validation for registering, not finished yet

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,36 @@
 from flask_wtf import FlaskForm
+import re
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
+
+
+def name_character_check(form, field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
+
+
+def character_check(form, field):
+    excluded_chars = "*?"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
-    pin_key = StringField()
-    submit = SubmitField()
+    email = StringField(validators=[Required(), Email()])
+    firstname = StringField(validators=[Required(), name_character_check])
+    lastname = StringField(validators=[Required(), name_character_check])
+    phone = StringField(validators=[Required()])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12,message="Password must be between 6 and 12 characters in length."),character_check()])
+    confirm_password = PasswordField(validators=[Required(), EqualTo("password", message="Both password fields must be equal.")])
+    pin_key = StringField(validators=[Required(), character_check, Length(max=32, min=32, message="Length of PIN key must be 32.")])
+    submit = SubmitField(validators=[Required()])
+
+    def validate_password(self, password):
+        p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*\W)')
+        if not p.match(self.password.data):
+            raise ValidationError("Password must contain at least 1 digit and 1 uppercase letter.")
