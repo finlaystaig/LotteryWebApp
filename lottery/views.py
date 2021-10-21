d4d66d6b5925a3f8b15e6e7db2c886d185fd8022
finlay <F.Staig1@newcastle.ac.uk>
2021-10-21 13:18:15 +0100

    added encryption for draws attempted to add decryption for these and failed spectacularly

--- lottery/views.py
+++ lottery/views.py
@@ -1,20 +1,34 @@
 # IMPORTS
+import copy
 import logging
 
 from flask import Blueprint, render_template, request, flash
 
 from app import db
-from models import Draw
+from models import Draw, User
 
 # CONFIG
 lottery_blueprint = Blueprint('lottery', __name__, template_folder='templates')
+
+user = User.query.first()
+draw_key = user.draw_key
 
 
 # VIEWS
 # view lottery page
 @lottery_blueprint.route('/lottery')
 def lottery():
-    return render_template('lottery.html')
+    draws = Draw.query.all()
+    draw_copies = list(map(lambda x: copy.deepcopy(x),draws))
+
+    decrypted_draws = []
+
+    print(draw_copies)
+    for d in draw_copies:
+        d.view_draw(draw_key)
+        decrypted_draws.append(d)
+
+    return render_template('lottery.html', draws=decrypted_draws)
 
 
 @lottery_blueprint.route('/add_draw', methods=['POST'])
@@ -25,7 +39,7 @@ def add_draw():
     submitted_draw.strip()
 
     # create a new draw with the form data.
-    new_draw = Draw(user_id=1, draw=submitted_draw, win=False, round=0)  # TODO: update user_id [user_id=1 is a placeholder]
+    new_draw = Draw(user_id=1, draw=submitted_draw, win=False, round=0, draw_key=draw_key)  # TODO: update user_id [user_id=1 is a placeholder]
 
     # add the new draw to the database
     db.session.add(new_draw)
@@ -41,6 +55,8 @@ def add_draw():
 def view_draws():
     # get all draws that have not been played [played=0]
     playable_draws = Draw.query.filter_by(played=False).all()  # TODO: filter playable draws for current user
+
+    print("playable draws",playable_draws)
 
     # if playable draws exist
     if len(playable_draws) != 0:
